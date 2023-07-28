dd90819c384ab3a6d97872a499f0d380769779e8
CarryARG <lucaas_carranza@hotmail.com>
2023-07-28 07:09:31 -0300

    primero, una resolvente para empezar

---
+++ archivo.py
@@ -0,0 +1,8 @@
+print("Hola mundo")
+a=2
+b=-1
+c=1
+resolvente_positivo= ((-b)+((b**2 - 4*a*c)**1/2))/(2*a)
+resolvente_negativo= ((-b)-((b**2 - 4*a*c)**1/2))/(2*a)
+print("el valor de la resolvente positiva es: ",resolvente_positivo)
+print("el valor de la resolvente negativa es: ",resolvente_negativo)
